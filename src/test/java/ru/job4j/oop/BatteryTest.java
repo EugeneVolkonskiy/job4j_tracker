3c405cf64be26abd06987174c947c24594cc4118
EugeneVolkonskiy <3376361@mail.ru>
2022-03-21 18:18:30 +0300

    1.7. Конструктор [#173889 #277733]

--- src/test/java/ru/job4j/oop/BatteryTest.java
+++ src/test/java/ru/job4j/oop/BatteryTest.java
@@ -1,8 +1,9 @@
-package ru.job4j.tracker.oop;
+package ru.job4j.oop;
 
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+
 import static org.hamcrest.Matchers.is;
 
 public class BatteryTest {
