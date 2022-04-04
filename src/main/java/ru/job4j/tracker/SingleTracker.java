1ffe8aa6c72639a286f8e17f2a92c69d423cc949
EugeneVolkonskiy <3376361@mail.ru>
2022-04-04 12:52:25 +0300

    1. Singleton [#94606 #283931]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,12 +2,15 @@ package ru.job4j.tracker;
 
 public final class SingleTracker {
 
-    private static Tracker tracker = new Tracker();
+    private Tracker tracker = null;
 
     private SingleTracker() {
     }
 
-    public static Tracker getTracker() {
+    public Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
         return tracker;
     }
 
