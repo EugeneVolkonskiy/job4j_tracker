27e61e162fba8ec689f61e1c0d650072bd80e3ff
EugeneVolkonskiy <3376361@mail.ru>
2023-08-03 15:14:25 +0300

    1. Профилирование. [#1591 # [#1591]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -33,12 +33,14 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        Store memTracker = new SqlTracker();
+        Store memTracker = new MemTracker();
         List<UserAction> actions = List.of(
                 new CreateAction(output),
+                new CreateManyItems(output),
                 new ShowAction(output),
                 new EditAction(output),
                 new DeleteAction(output),
+                new DeleteAllItems(output),
                 new FindByIdAction(output),
                 new FindByNameAction(output),
                 new ExitAction()
