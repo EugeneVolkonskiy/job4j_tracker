92347c8294c8041df3820c212e9dbb71cd0c9f60
EugeneVolkonskiy <3376361@mail.ru>
2024-02-06 16:58:35 +0300

    0. ToOne [#6873]

---
+++ src/main/java/ru/job4j/tracker/HQLUsage.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HQLUsage {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        try (SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory()) {
+            Session session = sf.openSession();
+           delete(session, 2102115);
+            session.close();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static void update(Session session, int id) {
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "UPDATE Item SET name = :fName WHERE id = :fId")
+                    .setParameter("fName", "new name")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public static void delete(Session session, int id) {
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE Item WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+}
